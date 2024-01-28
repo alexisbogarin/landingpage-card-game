ade5f891bdbdf9e19acebc81ec2f66377bbf8eb9
Alexis Bogarin <alexisbogarin61@gmail.com>
2024-01-28 20:32:53 -0300

    Actualizacion de css

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
         navLinks.classList.toggle('show');
     });
 });
+
+// Desplazamiento suave a todos los enlaces que apuntan a un ID
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
+  });
